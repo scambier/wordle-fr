0699fec33dd4e81866273987902bc0342059be15
Simon Cambier <simon.cambier@protonmail.com>
2025-01-27 20:22:36 +0100

    Correct url

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ process.env.VITE_BUILD_DATE = new Date().toISOString()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/motus2/',
+  base: '/momomotus/',
   plugins: [
     vue(),
     Icons({ autoInstall: true }),
